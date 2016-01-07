36d5bc3f6c67ac7cf53a2dbb31eea25d4edf8ad1
putnik_busy <maximus777.999@mail.ru>
2016-01-07 12:49:25 +0300

    делаю звук

--- app/src/main/java/com/just_app/diplom/Fragment_menu.java
+++ app/src/main/java/com/just_app/diplom/Fragment_menu.java
@@ -3,14 +3,19 @@ package com.just_app.diplom;
 
 import android.annotation.TargetApi;
 import android.app.Fragment;
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.AssetManager;
 import android.graphics.drawable.Drawable;
 import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.media.SoundPool;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -32,6 +37,10 @@ public class Fragment_menu extends Fragment {
     private Button forward, back;
     private TextView tv;
     private String mName;
+    private String mSound;
+    private MediaPlayer mp;
+    private SoundPool mSoundPool;
+    private int mTrack;
     private int i = 0;
 
     @Override
@@ -49,6 +58,7 @@ public class Fragment_menu extends Fragment {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 
     @Override
@@ -67,24 +77,23 @@ public class Fragment_menu extends Fragment {
             public void onClick(View v) {
                 switch ((v.getId())) {
                     case R.id.forward:
-                        if (i < mSubject.content.size()-1) {
+                        if (i < mSubject.content.size() - 1) {
                             i++;
                             OnChangePhoto(i);
-                        }else {
+                        } else {
                             i = 0;
                             OnChangePhoto(i);
                         }
 
-                        if(i==mSubject.content.size()-1){
+                        if (i == mSubject.content.size() - 1) {
                             OnChangePhoto(i);
                         }
                         break;
                     case R.id.back:
-                        if(i==0){
-                            i=mSubject.content.size()-1;
+                        if (i == 0) {
+                            i = mSubject.content.size() - 1;
                             OnChangePhoto(i);
-                        }else
-                        if (i <mSubject.content.size()){
+                        } else if (i < mSubject.content.size()) {
                             i--;
                             OnChangePhoto(i);
                         }
@@ -99,12 +108,15 @@ public class Fragment_menu extends Fragment {
     }
 
     public void OnChangePhoto(int i) {
+
+
         AssetManager mgr = getActivity().getAssets();
         if (mSubject.content != null) {
             try {
                 Uri imgUri = Uri.parse(mSubject.content.get(i).photos);
+                Uri soundUri = Uri.parse(mSubject.content.get(i).sounds);
                 mName = mSubject.content.get(i).signature;
-
+              //  mSound = mSubject.content.get(i).sounds;
                 InputStream stream = mgr.open(
                         imgUri.getPath().substring("/android_asset/".length())
                 );
@@ -112,10 +124,19 @@ public class Fragment_menu extends Fragment {
                 nameImage.setImageDrawable(drawableView);
                 tv.setText(mName);
 
+                if (mSound != null) {
+                    mp = new MediaPlayer();
+                    mp.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                    mp.setDataSource(getActivity().getApplicationContext(),soundUri);
+                    mp.prepare();
+                    mp.start();
+                }
 
             } catch (IOException e) {
                 e.printStackTrace();
+                Log.d("sound error", "sound error");
             }
         }
     }
+
 }
